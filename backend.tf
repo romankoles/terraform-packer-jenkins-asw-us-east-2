3a80c4152cc98820d0ac30fb6d2d3fe7c9f52ad9
Roman Koles <roman.koles@hotmail.com>
2020-08-11 16:22:04 -0400

    Add files via upload

---
+++ backend.tf
@@ -0,0 +1,7 @@
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-123abc321cba" # to be changed to rendered bucket
+#    key    = "terraform.tfstate"
+#    region = "us-east-2"
+#  }
+#}
